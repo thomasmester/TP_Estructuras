a47fc6aecccf28c4e4e40c696c92376bbd90334f
Manuel <manuejbe@gmail.com>
2023-04-07 16:11:20 -0300

    agregue clase empleado y actividad

--- clases.py
+++ clases.py
@@ -5,8 +5,22 @@ class Persona:
         self.sexo=sexo
         self.edad=edad
         self.DNI=DNI
+    def presentacion(self):
+        print("Me llamo {} {} y tengo {} años".format(self.nombre, self.apellido, self.edad))
 
 class Socio(Persona):
     def __init__(self, nombre, apellido, sexo, edad, DNI, nroSocio):
         super().__init__(nombre, apellido, sexo, edad, DNI)
         self.nroSocio=nroSocio
+
+class Empleado(Persona):
+    def __init__(self, nombre, apellido, sexo, edad, DNI, legajo):
+        super().__init__(nombre, apellido, sexo, edad, DNI)
+        self.legajo=legajo
+
+class Actividad:
+    def __init__(self, nombre, tipo):
+        self.nombre=nombre
+        self.tipo=tipo
+    def informacion(self):
+        print("La actividad {} es {}".format(self.nombre, self.tipo))
