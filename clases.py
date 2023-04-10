5aac2206d798caea6a213bb8955238df4cce4fbe
Manuel <manuejbe@gmail.com>
2023-04-10 17:33:29 -0300

    agregue clases

--- clases.py
+++ clases.py
@@ -1,3 +1,9 @@
+class Club:
+    def __init__(self, nombre, anioFundacion, direccion):
+        self.nombre=nombre
+        self.anioFundacion=anioFundacion
+        self.direccion=direccion
+
 class Persona:
     def __init__(self, nombre, apellido, sexo, edad, DNI):
         self.nombre=nombre
@@ -9,14 +15,17 @@ class Persona:
         print("Me llamo {} {} y tengo {} años".format(self.nombre, self.apellido, self.edad))
 
 class Socio(Persona):
-    def __init__(self, nombre, apellido, sexo, edad, DNI, nroSocio):
+    def __init__(self, nombre, apellido, sexo, edad, DNI, nroSocio, correoElectronico):
         super().__init__(nombre, apellido, sexo, edad, DNI)
         self.nroSocio=nroSocio
+        self.correoElectronico=correoElectronico
 
 class Empleado(Persona):
-    def __init__(self, nombre, apellido, sexo, edad, DNI, legajo):
+    def __init__(self, nombre, apellido, sexo, edad, DNI, legajo, cargo, salario):
         super().__init__(nombre, apellido, sexo, edad, DNI)
         self.legajo=legajo
+        self.cargo=cargo
+        self.salario=salario
 
 class Actividad:
     def __init__(self, nombre, tipo):
@@ -24,3 +33,30 @@ class Actividad:
         self.tipo=tipo
     def informacion(self):
         print("La actividad {} es {}".format(self.nombre, self.tipo))
+
+class Instalacion:
+    def __init__(self, nombre, descripcion, ubicacion, horaApertura, horaCierre):
+        self.nombre=nombre
+        self.descripcion=descripcion
+        self.ubicacion=ubicacion
+        self.horaApertura=horaApertura
+        self.horaCierre=horaCierre
+
+class Reserva:
+    def __init__(self, nombre, descripcion, ubicacion, horaApertura, horaCierre, fechaReserva, horaReserva):
+        super().__init__(nombre, descripcion, ubicacion, horaApertura, horaCierre)
+        self.fechaReserva=fechaReserva
+        self.horaReserva=horaReserva
+
+class Evento:
+    def __init__(self, nombre, descripcion, fecha, hora):
+        self.nombre=nombre
+        self.descripcion=descripcion
+        self.fecha=fecha
+        self.hora=hora
+
+class Pago:
+    def __init__(self, monto, fecha, nroSocio):
+        self.monto=monto
+        self.fecha=fecha
+        self.nroSocio=nroSocio
