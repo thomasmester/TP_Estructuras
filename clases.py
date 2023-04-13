f6ba9aac9feee0d193eff41747ae22e40a7102a3
SalvadorLuna912 <sluna@itba.edu.ar>
2023-04-13 19:56:56 -0300

    Update clases.py

--- clases.py
+++ clases.py
@@ -191,4 +191,38 @@ class Pago:
         self.monto = monto
         self.fecha = fecha
         self.socio=socio 
+<<<<<<< Updated upstream
         self.codigoPago = codigoPago
+=======
+        self.codigoPago = codigoPago      
+        
+
+def ingreso(archivo):
+    archivo = open(archivo,"a",encoding="utf-8")
+    print("1. Registrarse",'\n',"2. Iniciar sesión",'\n')
+    opcion = input("Ingrese la opción: ")
+    while opcion != 1 and opcion != 2:
+        opcion = input("Ingrese la opción: ")
+    if opcion == 1:
+        usuario = input("Ingrese usuario: ")
+        contrsenia = input("Ingrese contraseña: ")
+        archivo.write(usuario + " " + contrsenia + '\n')
+    else:
+        lista = []
+        otros = [" ",'\n']
+        for linea in archivo:
+            for elemento in linea:
+                if elemento not in otros:
+                    lista.append(elemento)
+        for i in range(len(lista)):
+            if lista[i] == usuario:
+                return lista[i+1] == contrsenia
+        
+
+        
+        
+
+def main():
+    
+    opcion = 
+>>>>>>> Stashed changes
